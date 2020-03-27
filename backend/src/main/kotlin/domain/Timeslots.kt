4706cfa095b49f916a1d8a69fccf463b49d7a293
Marko Milivojevic <marko.milivojevic@zuehlke.com>
2020-03-27 14:28:21 +0100

    Odradjene 1. i 2. stavka.   - U komandu yarn serve ubacen port 3000 i promenjen env za develop   - broj timeslot-ova se racuna tako sto se izdvoje svi timeslotovi za danasnji dan koji su nakon trenutnog vremena i pozove se count i to je to

--- backend/src/main/kotlin/domain/Timeslots.kt
+++ backend/src/main/kotlin/domain/Timeslots.kt
@@ -3,7 +3,9 @@ package domain
 import infra.DatabaseFactory.dbtx
 import model.TimeslotEntity
 import model.TimeslotsRepo
-import org.jetbrains.exposed.sql.deleteAll
+import org.jetbrains.exposed.sql.*
+import java.time.LocalDateTime
+
 
 @Target("Set of all timeslots.")
 object Timeslots {
@@ -18,8 +20,15 @@ object Timeslots {
 	/**
 	 * Counts available timeslots in the future.
 	 */
-	fun countAvailableTimeslots() {
-		TODO()
+	fun countAvailableTimeslots(): Long {
+		val currentDateTime = LocalDateTime.now()
+		val date = currentDateTime.year * 10000 + currentDateTime.monthValue * 100 + currentDateTime.dayOfMonth
+		val time = currentDateTime.hour * 100 + currentDateTime.minute
+
+		val query = TimeslotsRepo.selectAll()
+		query.andWhere { TimeslotsRepo.date eq date }
+		query.andWhere { TimeslotsRepo.time greater time }
+		return query.count()
 	}
 
 	private fun findExisting(id: Int): TimeslotEntity {
