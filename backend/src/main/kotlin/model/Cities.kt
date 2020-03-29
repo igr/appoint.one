75b7fbf4847a69ac91fc01550060c7ba3a01ffef
Marko Milivojevic <marko.milivojevic@zuehlke.com>
2020-03-29 19:48:32 +0200

    Added country in Cities table and added test for testing user login with wrong password

--- backend/src/main/kotlin/model/Cities.kt
+++ backend/src/main/kotlin/model/Cities.kt
@@ -5,6 +5,7 @@ import org.jetbrains.exposed.sql.Table
 object CitiesRepo : Table(name = "cities") {
 	val id = integer("id")
 	val name = varchar("name", 50)
+	val country = integer("country")
 
 	override val primaryKey = PrimaryKey(id, name = "id")
 }
