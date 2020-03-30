be0a6d0a1b380a4d5b254a441d2d0ff4c403901b
Marko Milivojevic <marko.milivojevic@zuehlke.com>
2020-03-30 22:09:30 +0200

    Tried to add tests, but there are more thins to do.

---
+++ backend/src/test/kotlin/web/DataApiKtTest.kt
@@ -0,0 +1,27 @@
+package web
+
+import common.ServerTest
+import model.NewDoctor
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class DataApiKtTest: ServerTest() {
+
+	@Test
+	fun `Get cities`() {
+		// TODO: Add logic here
+		// val cities = getCities()
+		// Assertions.assertThat(cities.size).isGreaterThan(0)
+	}
+
+
+	@Test
+	fun `Get countries`() {
+		// TODO: Add logic here
+		// val cities = getCities()
+		// Assertions.assertThat(cities.size).isGreaterThan(0)
+	}
+
+}
+
