d4373eec73a50c4d82b9bbee3a4287a7e2d77f2c
Igor Spasić <igor.spasic@gmail.com>
2020-03-31 10:17:05 +0200

    Login forma

--- frontend/src/views/login/index.vue
+++ frontend/src/views/login/index.vue
@@ -1,13 +1,56 @@
 <template>
-  <div class="login-container">
-    <a href="#" @click="handleLogin">login here</a><br>
-    <router-link to="/register">
-      Register
-    </router-link>
-    <div v-if="error">
-      {{ $t('login.error') }}
-    </div>
-  </div>
+  <v-row align="center" justify="center">
+    <v-col cols="12" sm="8" md="4">
+      <v-card class="elevation-12">
+        <v-toolbar color="#34558b" dark flat>
+          <v-toolbar-title>Login</v-toolbar-title>
+          <v-spacer />
+        </v-toolbar>
+        <v-card-text>
+          <v-form
+            ref="form"
+            v-model="valid"
+          >
+            <v-text-field
+              v-model="email"
+              label="email"
+              prepend-icon="person"
+              :rules="rules.email"
+              autocomplete="email"
+            />
+            <v-text-field
+              v-model="password"
+              label="Password"
+              prepend-icon="lock"
+              type="password"
+              :rules="rules.password"
+              autocomplete="current-password"
+            />
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            name="registre"
+            to="/register"
+          >
+            REGISTER
+          </v-btn>
+          <v-btn
+            name="submit"
+            :disabled="!valid"
+            @click="handleLogin"
+            color="primary"
+          >
+            LOGIN
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-alert v-if="error" type="error" dismissible>
+        {{ $t('login.error') }}
+      </v-alert>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
@@ -18,17 +61,32 @@ import { UserModule } from '@/store/modules/user';
   name: 'Login',
 })
 export default class extends Vue {
-  private loginForm = {
-    email: 'admin2',
-    password: '111111',
-  };
+  private email = '';
+
+  private valid = true;
+
+  private password = '';
 
   private redirect?: string;
 
   private error: boolean = false;
 
+  private rules = {
+    email: [
+      (v: string) => !!v || 'E-mail is required',
+      (v: string) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      (v: string) => !!v && /.+@.+\..+/.test(v),
+    ],
+    password: [
+      (v: string) => !!v || 'Password is required',
+    ],
+  };
+
   private async handleLogin() {
-    const success = await UserModule.Login(this.loginForm);
+    const success = await UserModule.Login({
+      email: this.email,
+      password: this.password,
+    });
     if (!success) {
       this.error = true;
       return;
