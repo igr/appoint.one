fd4f7499899bf3892272f6341018052f85eff2bb
Igor Spasić <igor.spasic@gmail.com>
2020-04-10 16:28:41 +0200

    Well, it is what it is.

--- backend/src/main/kotlin/server/Env.kt
+++ backend/src/main/kotlin/server/Env.kt
@@ -1,6 +1,6 @@
 package server
 
 object Env {
-	val REG_CODE = System.getenv("APP1_REG_CODE") ?: "A1"
+	val REG_CODE = System.getenv("APP1_REG_CODE") ?: "Dpts4477."
 	val JWT_SECRET = System.getenv("APP1_JWT_SECRET") ?: "xE1x1o1x8qflc1iYtcRd000OBLAC"
 }
